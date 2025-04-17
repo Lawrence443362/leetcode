9fb09a30164c38c02795e0599d7c26c90b0c8730
Lorenzo <kanlll443362@gmail.com>
2025-04-17 08:00:29 +0500

    Leetcode 283 - Move Zeroes

---
+++ 283.py
@@ -0,0 +1,10 @@
+# Move Zeroes
+class Solution(object):
+    def moveZeroes(self, nums):
+        slow = 0
+
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[fast], nums[slow] = nums[slow], nums[fast]
+                slow += 1
+
