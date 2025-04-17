682d3259a6fa76b5f09b84201e4b2ae510848df7
Lorenzo <kanlll443362@gmail.com>
2025-04-17 08:55:39 +0500

    optimize leetcode 88

--- 88.py
+++ 88.py
@@ -1,24 +1,18 @@
+# Merge Sorted Array
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        left = 0
-        right = 0
+        left = m - 1
+        right = n - 1
+        general = (n + m) - 1
 
-        left_list = nums1[:m] 
-        right_list = nums2[:n]
-
-        result = []
-
-        while left < len(left_list) and right < len(right_list):
-            if left_list[left] < right_list[right]:
-                result.append(left_list[left])
-                left += 1
+        while right >= 0:
+            if left >= 0 and nums1[left] > nums2[right]:
+                nums1[general] = nums1[left]
+                left -= 1
             else:
-                result.append(right_list[right])
-                right += 1
+                nums1[general] = nums2[right]
+                right -= 1
 
-        result.extend(left_list[left:m])
-        result.extend(right_list[right:n])
+            general -= 1
 
-        return result
 
-        
