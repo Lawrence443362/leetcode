61c1871d6cd873ec262776687607eea5c7d0a332
Lorenzo <kanlll443362@gmail.com>
2025-04-18 09:52:39 +0500

    # 1768. Merge Strings Alternately - improved

--- 1768.py
+++ 1768.py
@@ -1,22 +1,17 @@
 # 1768. Merge Strings Alternately
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        len1, len2 = len(word1), len(word2)
         result = ""
-        word1_len = len(word1)
-        word2_len = len(word2)
-        max_len = max(word1_len, word2_len)
-
-        if word2_len > word1_len:
-            for i in range(max_len):
-                if i < word1_len:
-                    result += word1[i] + word2[i]
-                else:
-                    result += word2[i]
-        else:
-            for i in range(max_len):
-                if i < word2_len:
-                    result += word1[i] + word2[i]
-                else:
-                    result += word1[i]
         
+        while i < max(len1, len2):
+            if i < len1:
+                result += word1[i]
+
+            if i < len2:
+                result += word2[i]
+
+            i += 1
+
         return result
