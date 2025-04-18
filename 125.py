db9a419a5c9c36ede5f5dfe2562d1b8d6cc66d96
Lorenzo <kanlll443362@gmail.com>
2025-04-18 17:38:52 +0500

    Remake Valid palindrome to two pointers

--- 125.py
+++ 125.py
@@ -2,8 +2,20 @@
 class Solution(object):
     def isPalindrome(self, string):
         string = string.lower()
-        only_letters = (c for c in string if c.isalnum())
-        result = "".join(only_letters)
+        left, right = 0, len(string) - 1
 
-        print(result)
-        return result == "".join(reversed(result))        
+        while left < right:
+            if string[left].isalnum() and string[right].isalnum():
+                if string[left] != string[right]:
+                    return False
+                else:
+                    left += 1
+                    right -= 1
+
+            if string[left].isalnum() == False:
+                left += 1
+
+            if string[right].isalnum() == False:
+                right -= 1
+
+        return True
