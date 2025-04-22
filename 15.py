fa7811ae908e8aaa866f24ccac92f8b4228c15be
Lorenzo <kanlll443362@gmail.com>
2025-04-22 09:26:00 +0500

    Refactored

--- 15.py
+++ 15.py
@@ -3,15 +3,16 @@ class Solution:
     def threeSum(self, nums):
         nums.sort()
         result = set()
+        n = len(nums)
         
-        for i in range(len(nums)):
+        for i in range(n):
             if nums[i] > 0:
-                break  # Все числа после будут положительными
+                break
 
             if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Пропускаем дубликаты
+                continue
 
-            left, right = i + 1, len(nums) - 1
+            left, right = i + 1, n - 1
             while left < right:
                 s = nums[i] + nums[left] + nums[right]
 
@@ -21,7 +22,7 @@ class Solution:
                     right -= 1
                 elif s < 0:
                     left += 1
-                else:
+                else: 
                     right -= 1
-        
+
         return list(result)
