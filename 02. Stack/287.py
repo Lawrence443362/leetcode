119cb819cfd3156ae16d3a4a7803e4ce7f996b9a
Lorenzo <kanlll443362@gmail.com>
2025-04-30 11:07:21 +0500

    Created optimal 287

--- 02. Stack/287.py
+++ 02. Stack/287.py
@@ -1,13 +1,21 @@
 # 287. Find the Duplicate Number
 class Solution:
     def findDuplicate(self, nums):
-        nums.sort()
-        slow, fast = 0, 1
-        n = len(nums)
+        slow, fast = nums[0], nums[0]
 
-        while fast < n:
-            if nums[slow] == nums[fast]:
-                return nums[fast]
-            else:
-                slow += 1
-                fast += 1
+        while True: 
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+        
+        
